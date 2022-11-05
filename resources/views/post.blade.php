46386d76eecaf3ef9eb6807991630a8b3c626ea2
fresaaaaa <fresabayu0601@gmail.com>
2022-11-05 20:16:11 +0700

    video ke 12

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,11 +3,24 @@
 
 
 @section('container')
-    <article>
-        <h2>{{ $post->title }}</h2>
-        <h5>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
-        {{ $post->body }}
-    </article>
+    
+    <div class="container"> 
+        <div class="row justify-content-center mb-5">
+        <div class="col-md-8">
+            <h1 class="mb-3">{{ $post->title }}</h1>
 
-    <a href="/blog" class="text-decoration-none d-block mt-3">Back to Blog</a>
+            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug  }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
+            <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid" alt="{{ $post->category->name }}">
+                
+            <article class="my-3 fs-5">
+                {!! $post->body !!}
+              </article>
+              
+                <a href="/blog" class="text-decoration-none d-block mt-3">Back to Blog</a>
+            </div>
+        </div>
+    </div>
+
+        
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
+        "active" => 'home',
         "title" => "Home"
 
     ]);
@@ -29,6 +30,7 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about',[
         "title" => "About",
+        "active" => 'about',
         "name" => "Fresabayu Anggoro",
         "email" => "fresabayu0601@gmail.com",
         "image" => "gon2.png"
@@ -42,6 +44,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function() {
     return view('categories',[
         'title' => 'Post Categories',
+        "active" => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -50,6 +53,7 @@ Route::get('/categories', function() {
 Route::get('/categories/{category:slug}', function(Category $category){
     return view('posts',[
         'title' => "Post By Category : $category->name",
+        "active" => 'categories',
         'posts' => $category->posts->load('category', 'author'),
     ]);
 });
