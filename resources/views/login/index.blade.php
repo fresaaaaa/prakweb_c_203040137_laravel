da1d66c243d61f279401e897e9ba84b2ab18b5c6
fresaaaaa <fresabayu0601@gmail.com>
2022-11-27 03:20:10 +0700

    menyelesaikan video ke 15

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -4,7 +4,15 @@
 @section('container')
 <div class="row justify-content-center">
     <div class="col-md-4">
-        <main class="form-signin w-100 m-auto">
+
+        @if(session()->has('success'))
+        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
+
+          <main class="form-signin w-100 m-auto">
             <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
             <form>
               <div class="form-floating">
