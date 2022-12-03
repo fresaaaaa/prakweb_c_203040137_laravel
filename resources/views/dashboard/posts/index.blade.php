f8bf31e2b4a8df25b39eb2374d3d8741ec58782c
fresaaaaa <fresabayu0601@gmail.com>
2022-12-04 04:17:44 +0700

    menyelesaikan video ke 20

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -8,7 +8,7 @@
 
 
     @if(session()->has('success'))
-        <div class="alert alert-success" role="alert">
+        <div class="alert alert-success col-lg-8" role="alert">
             {{ session('success') }}
         </div>
     @endif
@@ -33,8 +33,12 @@
               <td>{{ $post->category->name }}</td>
               <td>
                <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-               <a href="" class="badge bg-warning"><span data-feather="edit"></span></a>
-               <a href="" class="badge bg-danger"><span data-feather="x-circle"></span></a>
+               <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+               <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="badge bg-danger border-0" onclick="return confirm('Are you Sure?')"><span data-feather="x-circle"></span></button>
+                </form>
               </td>
             </tr>
             @endforeach
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,8 +7,12 @@
         <h1 class="mb-3">{{ $post->title }}</h1>
 
         <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my all post</a>
-        <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-        <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+        <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+        <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+            @method('delete')
+            @csrf
+            <button class="btn btn-danger" onclick="return confirm('Are you Sure?')"><span data-feather="x-circle"></span>Delete</button>
+        </form>
 
         <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid my-3 fs-5" alt="{{ $post->category->name }}">
 
